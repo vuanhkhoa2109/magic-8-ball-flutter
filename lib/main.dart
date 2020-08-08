d604c97d75ddde59ea3a0091d76b8083b6a111d4
Anh Khoa <68534134+vuanhkhoa2109@users.noreply.github.com>
2020-08-09 01:08:14 +0700

    feat: complete app
    Use app to random the answer for every questions HIHI

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,50 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() => runApp(EightBall());
+
+class EightBall extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          backgroundColor: Colors.blueAccent.shade700,
+          title: Text('Ask me everything'),
+        ),
+        body: BallPage(),
       ),
     );
+  }
+}
+
+class BallPage extends StatefulWidget {
+  @override
+  _BallPageState createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+  int ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        onPressed: () {
+          randomBallNumber();
+        },
+        child: Image(
+          image: AssetImage('images/ball$ballNumber.png'),
+        ),
+      ),
+    );
+  }
+
+  randomBallNumber() {
+    setState(() {
+      this.ballNumber = Random().nextInt(4) + 1;
+    });
+  }
+}
